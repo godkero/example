7022788ee38612a74b1f22b4b03f19ef1451d4fd
socmgr <socmgr@soc5w08.net>
2021-06-29 14:58:37 +0900

    ver6.29.0

--- control.v
+++ control.v
@@ -3,10 +3,11 @@
 module control
 #(
 parameter IDLE=3'b000,S0 = 3'b001,  S1 = 3'b010,
-	  S2 = 3'b011 , S3 = 3'b100, FINISH = 3'b101 
+	  S2 = 3'b011 , S3 = 3'b100, FINISH = 3'b101,
+	  ERROR = 3'b111 
 	)
 (
-	input 		    	clk,rst,start,
+	input 		    clk,rst,start,changed,
 	input      [2:0]    count,
 
 	output reg       	data_sel,clk_en,
@@ -19,14 +20,23 @@ parameter IDLE=3'b000,S0 = 3'b001,  S1 = 3'b010,
 
 	
 	always@*begin
-		case(state)
-			IDLE:      n_state = (start == 1'b1)? S0 : IDLE;
-			S0:        n_state = (3'b001 == count )? S1 : IDLE;
-			S1:        n_state = (3'b010 == count )? S2 : S1;
-			S2:        n_state = (3'b011 == count )? S3 : S2;
-			S3:    	   n_state = (3'b100 == count )? FINISH :S3 ;
-			FINISH:    n_state = (3'b101 == count )? IDLE : IDLE;
-			default   :n_state = IDLE;
+		case({state,changed})
+			{IDLE,1'b1}:      n_state = (start == 1'b1)? S0 : IDLE;
+			{IDLE,1'b0}:      n_state = (start == 1'b1)? S0 : IDLE;
+			{S0,1'b0}:        n_state = (3'b001 == count )? S1 : IDLE;
+			{S1,1'b0}:        n_state = (3'b010 == count )? S2 : S1;
+			{S2,1'b0}:        n_state = (3'b011 == count )? S3 : S2;
+			{S3,1'b0}:    	  n_state = (3'b100 == count )? FINISH :S3 ;
+			{FINISH,1'b0}:    n_state = (3'b101 == count )? IDLE : IDLE;
+			{S0,1'b1}:        n_state = ERROR;
+			{S1,1'b1}:        n_state = ERROR;
+			{S2,1'b1}:        n_state = ERROR;
+			{S3,1'b1}:    	  n_state = ERROR;
+			{ERROR,1'b1} :    n_state = ERROR;
+			{ERROR,1'b0} :    n_state = S0;
+			
+			default   :	  n_state = IDLE;
+		
 		endcase
 	end
 
@@ -39,12 +49,13 @@ parameter IDLE=3'b000,S0 = 3'b001,  S1 = 3'b010,
 	//output logic
 	always@(state)begin		
 		case(state)
-			IDLE:	begin sela = 1'bx;selb = 1'bx;sel_shifter = 2'bxx;done_flag = 1'b0;data_sel = 1'b1;clk_en = 1'b0;end
+			IDLE:	begin sela = 1'bx;selb = 1'bx;sel_shifter = 2'bxx;done_flag = 1'b0;data_sel = 1'b1;clk_en = 1'b1;end
 			S0:	begin sela = 1'b1;selb = 1'b1;sel_shifter = 2'b10;done_flag = 1'b0;data_sel = 1'b1;clk_en = 1'b1;end
 			S1:	begin sela = 1'b1;selb = 1'b0;sel_shifter = 2'b01;done_flag = 1'b0;data_sel = 1'b0;clk_en = 1'b1;end
 			S2:   	begin sela = 1'b0;selb = 1'b1;sel_shifter = 2'b01;done_flag = 1'b0;data_sel = 1'b0;clk_en = 1'b1;end
 			S3:    	begin sela = 1'b0;selb = 1'b0;sel_shifter = 2'b00;done_flag = 1'b0;data_sel = 1'b0;clk_en = 1'b1;end
 			FINISH:	begin sela = 1'bx;selb = 1'bx;sel_shifter = 2'bxx;done_flag = 1'b1;data_sel = 1'b1;clk_en = 1'b0;end
+			ERROR:	begin sela = 1'b1;selb = 1'b1;sel_shifter = 2'b10;done_flag = 1'b1;data_sel = 1'b0;clk_en = 1'b0;end
 		endcase
 	end
 
--- top.v
+++ top.v
@@ -10,7 +10,8 @@ module top
 	output  [7:0]   seg_position
 	
 );	
-
+	wire    [7:0]  a_in,b_in;
+	wire        	changed;
 	wire            clk_en;
 	wire    [2:0]   cnt;
 	wire 			sela,selb;
@@ -23,14 +24,22 @@ module top
 	wire  	[15:0]	value;
 	wire	[15:0] 	s_out;	
 	
+	detect_input dectector(.clk(clk),
+				.a(a),
+				.b(b),
+				.a_out(a_in),
+				.b_out(b_in),
+				.changed(changed)
+				);				
+	
 	//mux a,b
-	mux4 mux_a(.mux_in_a(a[7:4]),
+	mux4 mux_a(.mux_in_a(a_in[7:4]),
 			   .mux_in_b(a[3:0]),
 			   .mux_sel(sela), 
 			   .mux_out(aout)
 			   );
 	mux4 mux_b(.mux_in_a(b[7:4]),
-			   .mux_in_b(b[3:0]),
+			   .mux_in_b(b_in[3:0]),
 			   .mux_sel(selb), 
 			   .mux_out(bout)
 			   );
@@ -71,10 +80,11 @@ module top
 		     	 .state(state),
 	  	     	 .sela(sela),
 		     	 .selb(selb),
+			 .changed(changed),
 		     	 .sel_shifter(sel_shift),
 		     	 .done_flag(done_flag)
 		   );
-	counter6 counter(.clk(clk),
+	counter6 counter(.clk(clk),.changed(changed),
 		  			 .rst(rst),
 		  			 .start(start),
 				 	 .cnt(cnt)
